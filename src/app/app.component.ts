458b8498267424f04f7a44d125454312510b61f7
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-01-12 17:05:38 -0600

    created a component and added functions for the lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basics';
+  title = 'basics'
+  name = 'Luis';
+  imgURL = 'https://picsum.photos/id/237/500/500'
+
+  changeImage(e: KeyboardEvent) {
+    this.imgURL = (e.target as HTMLInputElement).value
+  }
+
+  getName() {
+    return this.name
+  }
+
+  logImg(event: string) {
+    console.log(event)
+  }
 }
