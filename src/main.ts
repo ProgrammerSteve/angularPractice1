46fba44020175a0ab4fd659aae03578f07d6a4a0
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-01-11 21:03:26 -0600

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+import { enableProdMode, isDevMode } from '@angular/core';
+
+//JIT compiler
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+//AOT compiler
+// import { platformBrowser } from '@angular/platform-browser';
+
 import { AppModule } from './app/app.module';
+
+if (!isDevMode()) {
+  enableProdMode();
+}
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
